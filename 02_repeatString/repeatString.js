a9cb3bc44d0e4728972d6ffea86c35cb87e94c7e
Mvp1931 <mihirphadnis001@gmail.com>
2024-09-29 08:08:20 +0530

    solved problems 01-03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (name, interval) {
+    let result = "";
+    if (interval < 0) {
+        return "ERROR";
+    } else {
+        while (interval > 0) {
+            result += name;
+            --interval;
+        }
+    }
+    return result;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
